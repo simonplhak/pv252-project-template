a6d57e6d4faf7d953511247e3e05e7bbc7cce2a0
Simon Plhak <plhak.s@gmail.com>
2024-09-23 16:49:11 +0200

    feat: more tests

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -1,5 +1,12 @@
-import { fibonacci } from "./fibonacci.ts";
+import { fibonacci, initFibonacciUi } from "./fibonacci.ts";
 
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
+});
+
+test("fibonacci-minus", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
 });
--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -5,3 +5,39 @@ test("find-watman", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
+
+
+test('navigate to SITE A', async ({ page }) => {  
+  await page.goto('/');
+  await page.getByText('Factorial value 5! is').click();
+  await expect(page).toHaveURL('http://127.0.0.1:8080/site_a.html')
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Site A' }).click();
+  await expect(page).toHaveURL('http://127.0.0.1:8080/site_a.html')
+});
+
+test('navigate to SITE B', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('5th fibonacci number is').click();
+  await expect(page).toHaveURL('http://127.0.0.1:8080/site_b.html')
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Site B' }).click();
+  await expect(page).toHaveURL('http://127.0.0.1:8080/site_b.html')
+});
+
+test('locate HOME', async ({ page }) => {  
+  await page.goto('/site_a.html');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page.getByText('This image is a static')).toBeInViewport();
+  await expect(page.getByText('5th fibonacci number is')).toBeInViewport();
+  await expect(page.getByText('Factorial value 5! is')).toBeInViewport();
+  await expect(page).toHaveURL('http://127.0.0.1:8080/')
+});
+
+test('locate alert', async ({ page }) => {
+  await page.goto('http://localhost:8080/');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page.getByRole('alert')).toBeVisible();
+  await page.getByRole('alert').click();
+  await expect(page.getByRole('alert')).not.toBeVisible();
+});
